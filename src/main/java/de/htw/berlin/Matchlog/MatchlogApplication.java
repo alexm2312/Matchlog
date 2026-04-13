2429e7228dbcb265580f29678efaedeb272e6254
alexm2312 <alexander.meier@student.htw-berlin.de>
2026-04-13 12:14:12 +0200

    First Commit

---
+++ src/main/java/de/htw/berlin/Matchlog/MatchlogApplication.java
@@ -0,0 +1,13 @@
+package de.htw.berlin.Matchlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htw/berlin/Matchlog/MatchlogApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.berlin.Matchlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
